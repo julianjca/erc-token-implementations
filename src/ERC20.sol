d7f9b9f24fa4a99d1f7cf12f18bf6bf1c0f6c2e6
Julian Anderson <juliancanderson@gmail.com>
2022-02-01 20:14:39 +0700

    chore: updating docs on ERC20

--- src/ERC20.sol
+++ src/ERC20.sol
@@ -1,21 +1,41 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.6;
 
+/// @notice an opinionated ERC20 implementation
+/// @title ERC20
+/// @author Julian <juliancanderson@gmail.com>
 contract ERC20 {
+    /*///////////////////////////////////////////////////////////////
+                            ERC20 METADATA
+    //////////////////////////////////////////////////////////////*/
     string private _name;
     string private _symbol;
     uint256 private _totalSupply;
     uint8 private _decimals = 18;
 
+    /*///////////////////////////////////////////////////////////////
+                                MAPPINGS
+    //////////////////////////////////////////////////////////////*/
     mapping(address => uint256) private _balances;
     mapping(address => mapping(address => uint256)) private _allowances;
 
     // https://ethereum.stackexchange.com/questions/8658/what-does-the-indexed-keyword-do
-    // The indexed parameters for logged events will allow you to search for these events using the indexed parameters as filters.
+    // The indexed parameters for logged events will
+    // allow you to search for these events using the indexed parameters as filters.
 
+    /*///////////////////////////////////////////////////////////////
+                                EVENTS
+    //////////////////////////////////////////////////////////////*/
     event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
 
+    /*///////////////////////////////////////////////////////////////
+                            CONSTRUCTOR
+    //////////////////////////////////////////////////////////////*/
     constructor(string memory name_, string memory symbol_) {
         _name = name_;
         _symbol = symbol_;
@@ -39,6 +59,50 @@ contract ERC20 {
 
     function balanceOf(address account) public view returns (uint256) {
         return _balances[account];
+    }
+
+    function transfer(address recipient, uint256 amount) public returns (bool) {
+        _transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) public returns (bool) {
+        _approve(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) public returns (bool) {
+        _transfer(sender, recipient, amount);
+
+        uint256 allowance = _allowances[sender][recipient];
+        require(
+            allowance >= amount,
+            "Allowance is lower than amount transfered"
+        );
+
+        _approve(sender, recipient, allowance - amount);
+
+        emit Transfer(sender, recipient, amount);
+        return true;
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                        INTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+    function _mint(address to, uint256 amount) internal virtual {
+        _totalSupply += amount;
+        _balances[to] += amount;
+        emit Transfer(address(0), to, amount);
+    }
+
+    function _burn(address from, uint256 amount) internal virtual {
+        _totalSupply -= amount;
+        _balances[from] -= amount;
+        emit Transfer(from, address(0), amount);
     }
 
     function _transfer(
@@ -68,44 +132,5 @@ contract ERC20 {
 
         _allowances[owner][spender] = amount;
         emit Approval(owner, spender, amount);
-    }
-
-    function transfer(address recipient, uint256 amount) public returns (bool) {
-        _transfer(msg.sender, recipient, amount);
-        return true;
-    }
-
-    function approve(address spender, uint256 amount) public returns (bool) {
-        _approve(msg.sender, spender, amount);
-        return true;
-    }
-
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) public returns (bool) {
-        _transfer(sender, recipient, amount);
-
-        uint256 allowance = _allowances[sender][recipient];
-        require(allowance >= amount, "Allowance is lower than amount transfered");
-
-        _approve(sender, recipient, allowance - amount);
-
-        emit Transfer(sender, recipient, amount);
-        return true;
-    }
-
-    // internal functions
-    function _mint(address to, uint256 amount) internal virtual {
-        _totalSupply += amount;
-        _balances[to] += amount;
-        emit Transfer(address(0), to, amount);
-    }
-
-    function _burn(address from, uint256 amount) internal virtual {
-        _totalSupply -= amount;
-        _balances[from] -= amount;
-        emit Transfer(from, address(0), amount);
     }
 }
