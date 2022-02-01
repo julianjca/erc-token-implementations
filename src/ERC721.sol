d74706fb1e2545bcf78e327ebf6fdd66f51bbba9
Julian Anderson <juliancanderson@gmail.com>
2022-02-01 21:02:28 +0700

    feat: adding internal _mint function

--- src/ERC721.sol
+++ src/ERC721.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.6;
 // custom errors
 error NotAuthorized();
 error WrongAddress();
+error TokenAlreadyMinted();
 
 /// @notice an opinionated ERC721 implementation
 /// @title ERC721
@@ -31,14 +32,14 @@ contract ERC721 {
                                 EVENTS
     //////////////////////////////////////////////////////////////*/
     event Transfer(
-        address indexed _from,
-        address indexed _to,
-        uint256 indexed _tokenId
+        address indexed from,
+        address indexed to,
+        uint256 indexed tokenId
     );
     event Approval(
         address indexed _owner,
         address indexed _approved,
-        uint256 indexed _tokenId
+        uint256 indexed tokenId
     );
     event ApprovalForAll(
         address indexed _owner,
@@ -58,65 +59,107 @@ contract ERC721 {
         return _balanceOf[account];
     }
 
-    function ownerOf(uint256 _tokenId) external view returns (address) {
-        return _ownerOf[_tokenId];
+    function ownerOf(uint256 tokenId) external view returns (address) {
+        return _ownerOf[tokenId];
     }
 
-    function approve(address _approved, uint256 _tokenId) external payable {
-        address tokenOwner = _ownerOf[_tokenId];
+    function approve(address approved, uint256 tokenId) external payable {
+        address tokenOwner = _ownerOf[tokenId];
 
         if (
-            tokenOwner != msg.sender || !isApprovedForAll[_approved][msg.sender]
+            tokenOwner != msg.sender || !isApprovedForAll[approved][msg.sender]
         ) {
             revert NotAuthorized();
         }
 
-        getApproved[_tokenId] = _approved;
+        getApproved[tokenId] = approved;
 
-        emit Approval(tokenOwner, _approved, _tokenId);
+        emit Approval(tokenOwner, approved, tokenId);
     }
 
-    function setApprovalForAll(address _operator, bool _approved) external {
-        isApprovedForAll[msg.sender][_operator] = _approved;
+    function setApprovalForAll(address operator, bool approved) external {
+        isApprovedForAll[msg.sender][operator] = approved;
 
-        emit ApprovalForAll(msg.sender, _operator, _approved);
+        emit ApprovalForAll(msg.sender, operator, approved);
     }
 
     /// @notice Transfer ownership of an NFT -- THE CALLER IS RESPONSIBLE
-    ///  TO CONFIRM THAT `_to` IS CAPABLE OF RECEIVING NFTS OR ELSE
+    ///  TO CONFIRM THAT `to` IS CAPABLE OF RECEIVING NFTS OR ELSE
     ///  THEY MAY BE PERMANENTLY LOST
     /// @dev Throws unless `msg.sender` is the current owner, an authorized
-    ///  operator, or the approved address for this NFT. Throws if `_from` is
-    ///  not the current owner. Throws if `_to` is the zero address. Throws if
-    ///  `_tokenId` is not a valid NFT.
-    /// @param _from The current owner of the NFT
-    /// @param _to The new owner
-    /// @param _tokenId The NFT to transfer
+    ///  operator, or the approved address for this NFT. Throws if `from` is
+    ///  not the current owner. Throws if `to` is the zero address. Throws if
+    ///  `tokenId` is not a valid NFT.
+    /// @param from The current owner of the NFT
+    /// @param to The new owner
+    /// @param tokenId The NFT to transfer
     function transferFrom(
-        address _from,
-        address _to,
-        uint256 _tokenId
-    ) external payable {
-        address owner = _ownerOf[_tokenId];
+        address from,
+        address to,
+        uint256 tokenId
+    ) public virtual {
+        address owner = _ownerOf[tokenId];
 
         // avoid transferring to the current owner or zero address
-        if (_to == owner || _to == address(0)) revert WrongAddress();
+        if (to == owner || to == address(0)) revert WrongAddress();
 
         if (
             msg.sender != owner ||
-            getApproved[_tokenId] != msg.sender ||
-            isApprovedForAll[_from][msg.sender]
+            getApproved[tokenId] != msg.sender ||
+            isApprovedForAll[from][msg.sender]
         ) {
             revert NotAuthorized();
         }
 
-        _ownerOf[_tokenId] = _to;
+        _ownerOf[tokenId] = to;
 
         unchecked {
-            _balanceOf[_from]--;
-            _balanceOf[_to]++;
+            _balanceOf[from]--;
+            _balanceOf[to]++;
         }
 
-        emit Transfer(_from, _to, _tokenId);
+        emit Transfer(from, to, tokenId);
     }
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public virtual {
+        transferFrom(from, to, tokenId);
+    }
+
+    function _mint(address to, uint256 tokenId) internal virtual {
+        if (to == address(0)) revert WrongAddress();
+        if (_ownerOf[tokenId] != address(0)) revert TokenAlreadyMinted();
+
+        _ownerOf[tokenId] = to;
+
+        unchecked {
+            _balanceOf[to]++;
+        }
+
+        emit Transfer(address(0), to, tokenId);
+    }
+}
+
+interface IERC721TokenReceiver {
+    /// @notice Handle the receipt of an NFT
+    /// @dev The ERC721 smart contract calls this function on the recipient
+    ///  after a `transfer`. This function MAY throw to revert and reject the
+    ///  transfer. Return of other than the magic value MUST result in the
+    ///  transaction being reverted.
+    ///  Note: the contract address is always the message sender.
+    /// @param _operator The address which called `safeTransferFrom` function
+    /// @param from The address which previously owned the token
+    /// @param tokenId The NFT identifier which is being transferred
+    /// @param _data Additional data with no specified format
+    /// @return `bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"))`
+    ///  unless throwing
+    function onERC721Received(
+        address _operator,
+        address from,
+        uint256 tokenId,
+        bytes calldata _data
+    ) external returns (bytes4);
 }
